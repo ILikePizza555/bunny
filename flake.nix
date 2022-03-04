1f13b387d3a866b8883f1f2e6469006d31558c3b
Izzy Lancaster <avrisaac555@gmail.com>
2022-03-03 18:02:55 -0800

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Tool for adding user interface elements to any shell.";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
+    fenix.url = github:nix-community/fenix;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = {self, nixpkgs, fenix, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      rust-package = fenix.packages.${system};
+    in
+      {
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          pname = "bunny";
+          version = "0.0.1";
+          src = ./.;
+          buildInputs = [
+            (rust-package.stable.withComponents [ 
+              "cargo"
+              "rustc"
+              "rustfmt"
+              "rust-src"
+            ])
+            rust-package.rust-analyzer
+          ];
+        };
+      }
+    );
+}
